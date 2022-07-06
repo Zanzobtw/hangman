1fd7a1919aa8f767e34a410b8b86b226bc9426c4
abudy <abudy@gmail.com>
2022-07-06 12:34:22 -0400

    Step 1 complete, user input is turned into a list

---
+++ main.py
@@ -0,0 +1,14 @@
+# Step 1:
+# get user input and turn it to a list
+
+user_input = input()                    # gets user input
+list_input = []                         # list called list_input
+
+for letter in user_input:               # for each letter in the users input
+    list_input.append(letter)           # add the letter to the list called list_input
+print(list_input)                       # print list_input
+
+for char in list_input:                 # for each character in list_input
+    if char == " ":                     # if the character is equal to a blank space
+        list_input.remove(char)         # remove the character from list_input
+print(list_input)                       # print list_input
