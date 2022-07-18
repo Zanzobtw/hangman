9b907c32f327d39c6ec3a6578ae17d08f43a4c64
abudy <abudy@gmail.com>
2022-07-18 12:26:29 -0400

    Added step 2 instructions, pushing to master

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ for char in list_input:                 # for each character in list_input
     if char == " ":                     # if the character is equal to a blank space
         list_input.remove(char)         # remove the character from list_input
 print(list_input)                       # print list_input
+
+# Step 2:
+# create list of random words and get
+# one word from the list to play on
