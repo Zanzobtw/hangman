65be8ec55570663d4ee3e97c389a0c37e4f8cc2f
Abdul <soccerplayer6764@gmail.com>
2022-07-19 13:00:38 -0400

    removed creation of user_input into a list, added onto step three

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import random
-# Step 1:
-# get user input and turn it to a list
-
-user_input = input()                    # gets user input
-list_input = []                         # list called list_input
-
-for letter in user_input:               # for each letter in the users input
-    list_input.append(letter)           # add the letter to the list called list_input
-print(list_input)                       # print list_input
-
-for char in list_input:                 # for each character in list_input
-    if char == " ":                     # if the character is equal to a blank space
-        list_input.remove(char)         # remove the character from list_input
-print(list_input)                       # print list_input
-
 # Step 2:
 # create list of random words and get
 # one word from the list to play on
 
-game_list = ["computer", "keyboard", "monitor"]
+game_list = ["computer", "keyboard", "monitor", "camera", "mouse", ""]          # create a list to be played on called game_list
+game_word = random.choice(game_list)                                            # set game_word to a random word from the game_list
+word_length = len(game_word)
+print(word_length * "_ " + "\n")
 
-print(random.choice(game_list))
+# Step 2:
+# get user input and turn it to a list
+
+user_input = input()                    # gets user input
+list_input = []                         # list for correct letters
+
+# Step 3:
+# look at user input and compare to game word
+
+if user_input != game_word:
+    for letter in user_input:
+        if letter in game_word:
+            list_input.append(letter)
+            print(letter + " is in the word" + '\n')
+            print("These are the correct letters guessed so far:", list_input)
+        else:
+            print(letter + " is not in the word" + '\n')
+else:
+    print("You guessed the word " + user_input + " and were correct!")
